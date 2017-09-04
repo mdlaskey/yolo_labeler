65e8eddc36b56269ccd0c89dcb1139748a764ab0
Autolab <mdlaskey@umich.edu>
2017-09-03 22:35:54 -0700

    added grasp detector

---
+++ detectors/grasp_detector.py
@@ -0,0 +1,163 @@
+import tensorflow as tf
+import numpy as np
+import os
+import glob
+import cv2
+import argparse
+import configs.config_bed as cfg
+from yolo.grasp_heat_net import GHNet
+from yolo.yolo_conv_features import YOLO_CONV
+from utils.timer import Timer
+import IPython
+import sys, os
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   # see issue #152
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
+slim = tf.contrib.slim
+from data_aug.draw_cross_hair import DrawPrediction
+
+class GDetector(object):
+
+    def __init__(self):
+        
+        
+
+        self.yc = YOLO_CONV(is_training = False)
+
+        self.classes = cfg.CLASSES
+        self.num_class = len(self.classes)
+        self.image_size = cfg.IMAGE_SIZE
+        self.cell_size = cfg.CELL_SIZE
+        self.boxes_per_cell = cfg.BOXES_PER_CELL
+        self.threshold = cfg.THRESHOLD
+        self.iou_threshold = cfg.IOU_THRESHOLD
+
+
+        self.boundary1 = self.cell_size * self.cell_size * self.num_class
+        self.boundary2 = self.boundary1 + self.cell_size * self.cell_size * self.boxes_per_cell
+
+        self.yc.load_network()
+        self.count = 0
+        self.dp = DrawPrediction()
+
+
+        #self.all_data = self.precompute_features(images)
+
+        self.load_trained_net()
+
+        #self.images_detectors()
+
+       
+
+    def load_trained_net(self):
+        self.sess = tf.Session()
+        self.sess.run(tf.global_variables_initializer())
+        self.net = GHNet(is_training = False)
+        trained_model_file = cfg.GRASP_OUTPUT_DIR+ cfg.NET_NAME
+        print 'Restoring weights from: ' + trained_model_file
+        self.variable_to_restore = slim.get_variables_to_restore()
+        count = 0
+        for var in self.variable_to_restore:
+            print str(count) + " "+ var.name
+            count += 1
+        
+        self.variables_to_restore = self.variable_to_restore[40:]
+        self.saver_f = tf.train.Saver(self.variables_to_restore, max_to_keep=None)
+
+        self.saver_f.restore(self.sess, trained_model_file)
+
+        
+    def precompute_features(self,images):
+
+        all_data = []
+        for image in images:
+
+            features = self.yc.extract_conv_features(image)
+
+            data = {}
+            data['image'] = image
+            data['features'] = features
+            all_data.append(data)
+
+        return all_data
+
+
+
+    def detect(self,inputs,image):
+        img_h, img_w, _ = image.shape
+        net_output = self.sess.run(self.net.logits,
+                                   feed_dict={self.net.images: inputs})
+        #IPython.embed()
+       
+        return net_output
+
+   
+
+
+    def get_grasp(self,image):
+       
+
+        features = self.yc.extract_conv_features(image)
+   
+        result = self.detect(features,image)
+       
+        
+        
+        x = cfg.T_IMAGE_SIZE_W*result[0,0]
+        y = cfg.T_IMAGE_SIZE_H*result[0,1]
+
+        pose = [x,y]
+
+        img = self.dp.draw_prediction(image,pose)
+        cv2.imshow('detected_result',img)
+        cv2.waitKey(30)
+
+
+        return pose
+
+    
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', default="YOLO_small.ckpt", type=str)
+    parser.add_argument('--weight_dir', default='weights', type=str)
+    parser.add_argument('--data_dir', default="data", type=str)
+    parser.add_argument('--gpu', default='', type=str)
+    args = parser.parse_args()
+
+    os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
+
+   
+    weight_file = "/home/autolab/Workspaces/michael_working/yolo_tensorflow/data/pascal_voc/output07_20_09_37_29save.ckpt-15000"
+    #weight_file = '/home/autolab/Workspaces/michael_working/yolo_tensorflow/data/pascal_voc/weights/save.ckpt-100'#os.path.join(args.data_dir, args.weight_dir, args.weights)
+    #weight_file = os.path.join(args.data_dir, args.weight_dir, args.weights)
+
+    imageList = glob.glob(os.path.join(cfg.IMAGE_PATH, '*.png'))
+    labelListOrig = glob.glob(os.path.join(cfg.LABEL_PATH, '*.p'))
+    labelList = [os.path.split(label)[-1].split('.')[0] for label in labelListOrig]
+    #remove labeled images
+    #imageList = [img for img in imageList if os.path.split(img)[-1].split('.')[0] not in labelList]
+
+    #imname = cfg.IMAGE_PATH + 'frame_1000.png'
+    #imname = 'test/person.jpg' 
+    #IPython.embed()
+    images = []
+    c = 0
+
+    detector = Detector(weight_file,images)
+    for imname in imageList:
+
+        detector.image_detector(imname)
+    
+
+    # detect from camera
+    # cap = cv2.VideoCapture(-1)
+    # detector.camera_detector(cap)
+
+    # detect from held out image file
+
+    
+
+
+if __name__ == '__main__':
+    main()
